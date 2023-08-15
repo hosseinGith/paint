925394340eeb1e7fc51a81ff91da9459f92afee8
hosseinGithu <133234861+hosseinGith@users.noreply.github.com>
2023-08-15 11:25:58 -0700

    Update javas.js

--- javascriptFiles/javas.js
+++ javascriptFiles/javas.js
@@ -59,8 +59,8 @@ if(navigator.platform === 'Win32'){
     function Drowing(e){
         if(!isDrowing)
         return
-        let x = e.touches[0].clientX
-        let y = e.touches[0].clientY
+        let x = e.touches[0].offsetX
+        let y = e.touches[0].offsetY
         ctx.lineWidth = brushRange.value
         ctx.strokeStyle = brushCol
         ctx.lineTo(x,y)
