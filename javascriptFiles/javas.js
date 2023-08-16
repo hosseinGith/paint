4d6a6cbb8d94ebafe791b21f6d573d5e324f8aed
hosseinGithu <133234861+hosseinGith@users.noreply.github.com>
2023-08-16 05:07:21 -0700

    Update javas.js

--- javascriptFiles/javas.js
+++ javascriptFiles/javas.js
@@ -7,17 +7,20 @@ const clear = document.querySelector('.clear')
 
 let ctx = canvas.getContext('2d')
 let isDrowing = false
-let brushWid = 5
+brushRange.value = 1
 let brushCol = ''
-function drow(e) {
-    isDrowing = true
-}
-canvas.width = canvas.offsetWidth
-canvas.height = canvas.offsetHeight
-window.addEventListener('resize', () => {
+function resz(){
     canvas.width = canvas.offsetWidth
     canvas.height = canvas.offsetHeight
-})
+}
+resz()
+
+//--------------- function for mouse envents --------------------
+
+function drow() {
+    isDrowing = true
+}
+
 function Drowing(e) {
     if (!isDrowing)
         return
@@ -28,29 +31,13 @@ function Drowing(e) {
     ctx.lineTo(x, y)
     ctx.stroke()
 }
+
 function endDrawing() {
     ctx.beginPath()
     isDrowing = false
 }
-canvas.addEventListener('mousedown', drow)
-canvas.addEventListener('mousemove', Drowing)
-canvas.addEventListener('mouseup', endDrawing)
-canvas.addEventListener('mouseleave', endDrawing)
-brushColor.addEventListener('change', () => {
-    brushCol = brushColor.value
-})
-brush.addEventListener('click', () => {
-    brushCol = brushColor.value
-})
-eraser.addEventListener('click', () => {
-    brushCol = '#fff'
-})
-clear.addEventListener('click', () => {
-    ctx.fillStyle = '#fff'
-    ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
-})
 
-
+//--------------- function for touch envent --------------------
 
 function DrowingPhone(e) {
     let x = e.touches[0].clientX - canvas.offsetLeft
@@ -61,12 +48,16 @@ function DrowingPhone(e) {
     ctx.lineTo(x, y)
     ctx.stroke()
 }
+
 function endDrawingPhone() {
     ctx.beginPath()
 }
+//--------------- envents for mouse --------------------
 
-canvas.addEventListener('touchmove', DrowingPhone)
-canvas.addEventListener('touchend', endDrawingPhone)
+canvas.addEventListener('mousedown', drow)
+canvas.addEventListener('mousemove', Drowing)
+canvas.addEventListener('mouseup', endDrawing)
+canvas.addEventListener('mouseleave', endDrawing)
 
 brushColor.addEventListener('change', () => {
     brushCol = brushColor.value
@@ -81,3 +72,13 @@ clear.addEventListener('click', () => {
     ctx.fillStyle = '#fff'
     ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
 })
+
+
+//--------------- envents for touch --------------------
+
+canvas.addEventListener('touchmove', DrowingPhone)
+canvas.addEventListener('touchend', endDrawingPhone)
+
+//----------if the ueser change the window size ,the canvas will refresh the self offset width----- 
+
+window.addEventListener('resize',resz)
