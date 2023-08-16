c027800e07070adc758197194a25c5c6eb6c9abd
hosseinGithu <133234861+hosseinGith@users.noreply.github.com>
2023-08-16 03:06:08 -0700

    Update javas.js

--- javascriptFiles/javas.js
+++ javascriptFiles/javas.js
@@ -9,83 +9,75 @@ let ctx = canvas.getContext('2d')
 let isDrowing = false
 let brushWid = 5
 let brushCol = ''
-if(navigator.platform === 'Win32'){
-    function drow(e){
-        isDrowing = true
-    }
-    canvas.width = canvas.offsetWidth
-    canvas.height = canvas.offsetHeight
-    window.addEventListener('resize',()=>{
-        canvas.width = canvas.offsetWidth
-        canvas.height = canvas.offsetHeight
-    })
-    function Drowing(e){
-        if(!isDrowing)
-        return
-        let x = e.offsetX
-        let y = e.offsetY
-        ctx.lineWidth = brushRange.value
-        ctx.strokeStyle = brushCol
-        ctx.lineTo(x,y)
-        ctx.stroke()
-    }
-    function endDrawing(){
-        ctx.beginPath()
-        isDrowing = false
-    }
-    canvas.addEventListener('mousedown',drow)
-    canvas.addEventListener('mousemove',Drowing)
-    canvas.addEventListener('mouseup',endDrawing)
-    canvas.addEventListener('mouseleave',endDrawing)
-    brushColor.addEventListener('change',()=>{
-        brushCol = brushColor.value
-    })
-    brush.addEventListener('click',()=>{
-        brushCol = brushColor.value
-    })
-    eraser.addEventListener('click',()=>{
-        brushCol = '#fff'
-    })
-    clear.addEventListener('click',()=>{
-        ctx.fillStyle = '#fff'
-        ctx.fillRect(0,0,canvas.offsetWidth,canvas.offsetHeight)
-    })
-}else{
-    function drow(){
-        isDrowing = true
-    }
-    canvas.width = canvas.offsetWidth
-    canvas.height = canvas.offsetHeight
-    function Drowing(e){
-        if(!isDrowing)
-        returoffset    
-        let x = e.touches[0].clientX - canvas.offsetLeft
-        let y = e.touches[0].clientY - canvas.offsetTop
-        ctx.lineWidth = brushRange.value
-        ctx.strokeStyle = brushCol
-        ctx.lineTo(x,y)
-        ctx.stroke()
-    }
-    function endDrawing(){
-        ctx.beginPath()
-        isDrowing = false
-    }
-    canvas.addEventListener('touchstart',drow)
-    canvas.addEventListener('touchmove',Drowing)
-    canvas.addEventListener('touchend',endDrawing)
-    canvas.addEventListener('mouseleave',endDrawing)
-    brushColor.addEventListener('change',()=>{
-        brushCol = brushColor.value
-    })
-    brush.addEventListener('click',()=>{
-        brushCol = brushColor.value
-    })
-    eraser.addEventListener('click',()=>{
-        brushCol = '#fff'
-    })
-    clear.addEventListener('click',()=>{
-        ctx.fillStyle = '#fff'
-        ctx.fillRect(0,0,canvas.offsetWidth,canvas.offsetHeight)
-    })
-
+function drow(e) {
+    isDrowing = true
 }
+canvas.width = canvas.offsetWidth
+canvas.height = canvas.offsetHeight
+window.addEventListener('resize', () => {
+    canvas.width = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
+})
+function Drowing(e) {
+    if (!isDrowing)
+        return
+    let x = e.offsetX
+    let y = e.offsetY
+    ctx.lineWidth = brushRange.value
+    ctx.strokeStyle = brushCol
+    ctx.lineTo(x, y)
+    ctx.stroke()
+}
+function endDrawing() {
+    ctx.beginPath()
+    isDrowing = false
+}
+canvas.addEventListener('mousedown', drow)
+canvas.addEventListener('mousemove', Drowing)
+canvas.addEventListener('mouseup', endDrawing)
+canvas.addEventListener('mouseleave', endDrawing)
+brushColor.addEventListener('change', () => {
+    brushCol = brushColor.value
+})
+brush.addEventListener('click', () => {
+    brushCol = brushColor.value
+})
+eraser.addEventListener('click', () => {
+    brushCol = '#fff'
+})
+clear.addEventListener('click', () => {
+    ctx.fillStyle = '#fff'
+    ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
+})
+
+
+
+function DrowingPhone(e) {
+
+    let x = e.touches[0].clientX - canvas.offsetLeft
+    let y = e.touches[0].clientY - canvas.offsetTop
+    ctx.lineWidth = brushRange.value
+    ctx.strokeStyle = brushCol
+    ctx.lineTo(x, y)
+    ctx.stroke()
+}
+function endDrawingPhone() {
+    ctx.beginPath()
+    isDrowing = false
+}
+canvas.addEventListener('touchmove', DrowingPhone)
+canvas.addEventListener('touchend', endDrawingPhone)
+
+brushColor.addEventListener('change', () => {
+    brushCol = brushColor.value
+})
+brush.addEventListener('click', () => {
+    brushCol = brushColor.value
+})
+eraser.addEventListener('click', () => {
+    brushCol = '#fff'
+})
+clear.addEventListener('click', () => {
+    ctx.fillStyle = '#fff'
+    ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
+})
