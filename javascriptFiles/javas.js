aca1389a2175cf7ec49e05b472608b8ff33c958c
hossein derees <hosseinderees7@gmail.com>
2023-10-27 09:35:36 +0330

    i fixed the touch drawing

--- javascriptFiles/javas.js
+++ javascriptFiles/javas.js
@@ -1,84 +1,96 @@
-const canvas = document.querySelector('canvas')
-const brush = document.querySelector('.brush')
-const eraser = document.querySelector('.eraser')
-const brushColor = document.querySelector('.brushColor')
-const brushRange = document.querySelector('.brushRange')
-const clear = document.querySelector('.clear')
+const canvas = document.querySelector("canvas");
+const brush = document.querySelector(".brush");
+const eraser = document.querySelector(".eraser");
+const brushColor = document.querySelector(".brushColor");
+const brushRange = document.querySelector(".brushRange");
+const clear = document.querySelector(".clear");
+const save = document.querySelector(".saveBtn");
 
-let ctx = canvas.getContext('2d')
-let isDrowing = false
-brushRange.value = 1
-let brushCol = ''
+let ctx = canvas.getContext("2d");
+let isDrowing = false;
+brushRange.value = 1;
+let brushCol = "";
 function resz() {
-    canvas.width = canvas.offsetWidth
-    canvas.height = canvas.offsetHeight
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
-resz()
+resz();
 
 //--------------- function for mouse envents --------------------
 
-function drow() {
-    isDrowing = true
+function drow(e) {
+  e.preventDefault();
+  isDrowing = true;
 }
 
 function Drowing(e) {
-    if (!isDrowing)
-        return
-    let x = e.offsetX
-    let y = e.offsetY
-    ctx.lineWidth = brushRange.value
-    ctx.strokeStyle = brushCol
-    ctx.lineTo(x, y)
-    ctx.stroke()
+  e.preventDefault();
+  if (!isDrowing) return;
+  let x = e.offsetX;
+  let y = e.offsetY;
+  ctx.lineWidth = brushRange.value;
+  ctx.strokeStyle = brushCol;
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 
-function endDrawing() {
-    ctx.beginPath()
-    isDrowing = false
+function endDrawing(e) {
+  e.preventDefault();
+  ctx.beginPath();
+  isDrowing = false;
 }
 
 //--------------- function for touch envent --------------------
 
 function DrowingPhone(e) {
-    let x = e.touches[0].clientX - canvas.offsetLeft
-    let y = e.touches[0].clientY - canvas.offsetTop
+  e.preventDefault();
+  let x = e.touches[0].clientX - canvas.offsetLeft;
+  let y = e.touches[0].clientY - canvas.offsetTop;
 
-    ctx.lineWidth = brushRange.value
-    ctx.strokeStyle = brushCol
-    ctx.lineTo(x, y)
-    ctx.stroke()
+  ctx.lineWidth = brushRange.value;
+  ctx.strokeStyle = brushCol;
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 
 function endDrawingPhone() {
-    ctx.beginPath()
+  ctx.beginPath();
 }
 //--------------- envents for mouse --------------------
 
-canvas.addEventListener('mousedown', drow)
-canvas.addEventListener('mousemove', Drowing)
-canvas.addEventListener('mouseup', endDrawing)
-canvas.addEventListener('mouseleave', endDrawing)
+canvas.addEventListener("mousedown", drow);
+canvas.addEventListener("mousemove", Drowing);
+canvas.addEventListener("mouseup", endDrawing);
+canvas.addEventListener("mouseleave", endDrawing);
 
-brushColor.addEventListener('change', () => {
-    brushCol = brushColor.value
-})
-brush.addEventListener('click', () => {
-    brushCol = brushColor.value
-})
-eraser.addEventListener('click', () => {
-    brushCol = '#fff'
-})
-clear.addEventListener('click', () => {
-    ctx.fillStyle = '#fff'
-    ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
-})
-
-
+brushColor.addEventListener("change", () => {
+  brushCol = brushColor.value;
+});
+brush.addEventListener("click", (e) => {
+  brushCol = brushColor.value;
+});
+eraser.addEventListener("click", () => {
+  brushCol = "#fff";
+});
+clear.addEventListener("click", () => {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+});
+save.addEventListener("click", () => {
+  let link = document.createElement("a");
+  link.download = Math.random().toString().substring(2, 9);
+  link.href = canvas.toDataURL();
+  link.click();
+});
 //--------------- envents for touch --------------------
 
-canvas.addEventListener('touchmove', DrowingPhone)
-canvas.addEventListener('touchend', endDrawingPhone)
+canvas.addEventListener("touchmove", DrowingPhone);
+canvas.addEventListener("touchend", endDrawingPhone);
+canvas.addEventListener("touchstart", () => {
+  console.log(1);
+});
+//----------if the user change the window size ,the canvas will refresh the self offset width-----
 
-//----------if the ueser change the window size ,the canvas will refresh the self offset width----- 
-
-window.addEventListener('resize', resz)
+window.addEventListener("resize", resz);
